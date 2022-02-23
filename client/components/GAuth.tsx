bb94705479dda59c11273af036abc3febd606ce5
gabrielcastrosilva <gabriel.silva.010@acad.pucrs.br>
2022-02-23 13:43:54 -0300

    GSuite auth implementation

--- client/components/GAuth.tsx
+++ client/components/GAuth.tsx
@@ -1,40 +1,44 @@
 import React from 'react';
 import { GoogleLogin } from 'react-google-login'
+import GoogleButton from 'react-google-button'
 import { refreshTokenSetup } from './utils/refreshToken'
-
-import { useCallback, useState } from "react";
-
-
-const clientId = '599605619584-shbhg17403n9gc49afij6ljb92qn8omk.apps.googleusercontent.com';
+import getConfig from "next/config";
 
 
-function Login({parentCallback}) {
+const { publicRuntimeConfig } = getConfig();
+
+function Login({ parentCallback }) {
 
     const onSuccess = (res) => {
-        console.log('[Login Succes] currentUser: ', res.profileObj);
-        console.log(res)
+        //Sees profile object that google returns
+        //console.log('[Login Succes] currentUser: ', res.profileObj);
 
-        refreshTokenSetup(res)
-
-        parentCallback(res.profileObj)
+        refreshTokenSetup(res) //Refreshes tokens so it doesnt auto-logout
+        parentCallback(res.profileObj) //Returns profile obj
     };
 
     const onFailure = (res) => {
-        console.log('[Login Failed] res: ', res);
+        console.log('[Login Failed] res: ', res); // Error message
     };
-
-;
 
     return (
         <div>
             <GoogleLogin
-            clientId={clientId}
-            buttonText="Login"
-            onSuccess={onSuccess}
-            onFailure={onFailure}
-            cookiePolicy={'single_host_origin'}
-            style={{marginTop: '100px'}}
-            isSignedIn={true}
+                clientId={publicRuntimeConfig.GOOGLE_CLIENT_ID}
+                buttonText="Login"
+                onSuccess={onSuccess}
+                onFailure={onFailure}
+                cookiePolicy={'single_host_origin'}
+                render={renderProps => (
+                    <GoogleButton
+                        style={{ width: "100%", marginTop: 50 }}
+                        onClick={renderProps.onClick}
+                        disabled={renderProps.disabled}
+                    >
+                        Sign in with Google
+                    </GoogleButton>
+                )}
+                isSignedIn={true}
             />
         </div>
     )
