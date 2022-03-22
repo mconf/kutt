440dc831cf1f125825551efb0f0b551ac2c08abf
akahenry <henrybkavila@gmail.com>
2022-03-22 09:32:50 -0300

    Fix indentation on refreshToken script

--- client/components/utils/refreshToken.ts
+++ client/components/utils/refreshToken.ts
@@ -1,5 +1,5 @@
 export const refreshTokenSetup = res => {
-    const refreshTiming = (res.tokenObj.expires_in || 3600 - 5 * 60) * 1000;
+  const refreshTiming = (res.tokenObj.expires_in || 3600 - 5 * 60) * 1000;
 
-    setInterval(res.reloadAuthResponse, refreshTiming);
-  };
+  setInterval(res.reloadAuthResponse, refreshTiming);
+};
